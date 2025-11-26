5fca397cf6eff17f30f29273b8057484c1ec2eb4
Samuel Richardson (richardsos) <richardsos@coventry.ac.uk>
2025-11-26 07:14:34 +0000

    A Star algorithm applied to the grid and can be visualised. End node can be found from start node whilst taking barriers into account.

--- a_star.py
+++ a_star.py
@@ -55,14 +55,12 @@ class Node:
     def start_it(self):
         self.colour = CITRINE
 
-        
-
-    def close_it(self):
-        self.colour == SALMON
-    
     def open_it(self):
-        self.colour == EMERALD
-    
+        self.colour = EMERALD
+        
+    def close_it(self):
+        self.colour = SALMON
+  
     def block_it(self):
         self.colour = MOONSTONE
        
@@ -76,7 +74,20 @@ class Node:
         pygame.draw.rect(window, self.colour, (self.x, self.y, self.width, self.width))
 
     def update_neighbors(self, grid):
-        pass
+        
+        self.neighbors = []
+        if self.row < self.total_rows - 1 and not grid[self.row + 1][self.col].is_blocked(): #DOWN
+            self.neighbors.append(grid[self.row + 1][self.col])
+
+        if self.row > 0 and not grid[self.row - 1][self.col].is_blocked(): #UP
+            self.neighbors.append(grid[self.row - 1][self.col])
+
+        if self.col < self.total_rows - 1 and not grid[self.row ][self.col + 1].is_blocked(): #RIGHT
+            self.neighbors.append(grid[self.row][self.col + 1])
+
+        if self.row > 0 and not grid[self.row][self.col - 1].is_blocked(): #LEFT
+            self.neighbors.append(grid[self.row][self.col - 1])
+
 
     def is_less_than(self, other):
         return False
@@ -85,7 +96,58 @@ class Node:
 def h(p1, p2):
         x1, y1 = p1
         x2, y2 = p2
-        return abs(x1 - x2 + abs(y1 - y2))    
+        return abs(x1 - x2 + abs(y1 - y2))   
+
+def algorithm(draw, grid, start, end):
+	count = 0
+	open_set = PriorityQueue()
+	open_set.put((0, count, start))
+	came_from = {}
+	g_score = {spot: float("inf") for row in grid for spot in row}
+	g_score[start] = 0
+	f_score = {spot: float("inf") for row in grid for spot in row}
+	f_score[start] = h(start.get_position(), end.get_position())
+
+	open_set_hash = {start}
+
+	while not open_set.empty():
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				pygame.quit()
+
+		current = open_set.get()[2]
+		open_set_hash.remove(current)
+
+		if current == end:
+			pass
+			return True
+
+		for neighbor in current.neighbors:
+			temp_g_score = g_score[current] + 1
+
+			if temp_g_score < g_score[neighbor]:
+				came_from[neighbor] = current
+				g_score[neighbor] = temp_g_score
+				f_score[neighbor] = temp_g_score + h(neighbor.get_position(), end.get_position())
+				if neighbor not in open_set_hash:
+					count += 1
+					open_set.put((f_score[neighbor], count, neighbor))
+					open_set_hash.add(neighbor)
+					neighbor.open_it()
+                    
+
+		draw()
+
+		if current != start:
+			current.close_it()
+        
+
+	return False
+
+                
+
+       
+      
     
 def make_grid(rows, width):
         grid = []
@@ -106,7 +168,7 @@ def draw_grid(window, rows, width):
         pygame.draw.line(window, BLACK, (j * gap, 0), (j * gap, width))
   
 
-def draw_all(window, grid, rows, width):
+def draw(window, grid, rows, width):
     window.fill(WHITE)
 
     for row in grid:
@@ -134,17 +196,13 @@ def main(window, width):
     end = None
 
     run = True
-    started = False
     while run:
-        draw_all(window, grid, ROWS, width)
+        draw(window, grid, ROWS, width)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
             
-            if started:
-                continue
-
-            if pygame.mouse.get_pressed()[0]: #Left Click
+            if pygame.mouse.get_pressed()[0]: #LEFT CLICK
                 pos = pygame.mouse.get_pos()
                 row, col = get_pos_clicked(pos, ROWS, width)
                 node = grid[row][col]
@@ -161,7 +219,7 @@ def main(window, width):
                     
                 
             
-            elif pygame.mouse.get_pressed()[2]: #Right Click
+            elif pygame.mouse.get_pressed()[2]: #RIGHT CLICK
                 pos = pygame.mouse.get_pos()
                 row, col = get_pos_clicked(pos, ROWS, width)
                 node = grid[row][col]
@@ -170,10 +228,22 @@ def main(window, width):
                     start = None
                 elif node == end:
                     end = None
-                
 
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE and start and end:
+                    for row in grid:
+                        for node in row:
+                            node.update_neighbors(grid)
 
+                    algorithm(lambda: draw(window, grid, ROWS, width), grid, start, end)
 
+				    
+							
+						
+					
+			    
+
+            
     
     pygame.quit()
 
